9f38ed63aa2d09ee7ebb8290e13a7e040b24b297
liugaowei999 <187346454@qq.com>
2017-12-06 17:06:32 +0800

    update

---
+++ springmvc/src/main/java/com/liugw/learn/jdbc/UserModel.java
@@ -0,0 +1,23 @@
+package com.liugw.learn.jdbc;
+
+public class UserModel {
+	private int id;
+	private String myName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMyName() {
+		return myName;
+	}
+
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+
+}
